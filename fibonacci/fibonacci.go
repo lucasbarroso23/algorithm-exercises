a613cb1d9cdb3548aaf907de3b8b77f325fef645
lucasbarroso23 <lucasbarroso23@gmail.com>
2024-03-25 11:24:31 +0000

    fibonacci using go routines and select

--- fibonacci/fibonacci.go
+++ fibonacci/fibonacci.go
@@ -1,5 +1,7 @@
 package fibonacci
 
+import "fmt"
+
 /*
 The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence,
 such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
@@ -44,4 +46,18 @@ func fibonacciGoRoutine(c chan int) {
 		x, y = y, x+y
 	}
 	close(c)
+}
+
+func fibonacciGoRoutineSelect(c, quit chan int) {
+	x, y := 0, 1
+
+	for {
+		select {
+		case c <- x:
+			x, y = y, x+y
+		case <-quit:
+			fmt.Println("quit")
+			return
+		}
+	}
 }
--- fibonacci/fibonacci_test.go
+++ fibonacci/fibonacci_test.go
@@ -26,6 +26,18 @@ func TestFibonacci(t *testing.T) {
 		for i := range c {
 			fmt.Print(i)
 		}
+	})
 
+	t.Run("fibonacciGoRoutineSelect", func(t *testing.T) {
+		c := make(chan int)
+		quit := make(chan int)
+
+		go func() {
+			for i := 0; i < 10; i++ {
+				fmt.Print(<-c)
+			}
+			quit <- 0
+		}()
+		fibonacciGoRoutineSelect(c, quit)
 	})
 }
